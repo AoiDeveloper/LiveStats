7c48c41b80a7ac00cab3b8ceabe5073121572cf2
MakiZushi <aoibotowner@gmail.com>
2025-07-09 17:07:33 +0900

    initial commit

---
+++ src/main/kotlin/net/tsubu/liveStats/LiveStats.kt
@@ -0,0 +1,14 @@
+package net.tsubu.liveStats
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LiveStats : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
